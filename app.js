d2029dfe1e2f7505b6de471c7a0239da4e2eb69c
shruti <sgangras@equalexperts.com>
2014-10-09 23:43:46 -0700

    [SG] - added stream/:id/batch

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var crypto = require('crypto');
 var moment = require("moment");
 var uuid = require('node-uuid');
 var ObjectId = require('mongodb').ObjectID;
+var _ = require('underscore');
+var Q = require('q');
 
 var app = express();
 
@@ -66,7 +68,7 @@ app.get('/stream/:id', function (req, res) {
         }
   });
 });
-
+//TODO:clean it the way its done /batch
 app.post('/stream/:id/event', function (req, res) {
     var writeToken = req.headers.authorization;
     authenticateWriteToken(
@@ -76,12 +78,46 @@ app.post('/stream/:id/event', function (req, res) {
           res.status(404).send("stream not found");
       },
       function (stream) {
-          var parsedEvent = parseEvent(req.body, stream,res)
+          var parsedEvent = parseEvent(req.body, stream)
           saveEventToFile(parsedEvent,stream.streamid);
           res.send(JSON.stringify(parsedEvent))
       }
     );
  });
+
+ app.post('/stream/:id/batch', function(req,res){
+   var writeToken = req.headers.authorization;
+   var streamid = req.params.id;
+   var events = req.body;
+   var eventsToSend = [];
+
+    findStreamById(streamid,function(err,stream){
+        if(writeToken == stream.writeToken){
+           var parsedEvents = parseEvents(events,stream);
+            saveEventsToFile(parsedEvents,stream.streamid)
+           .then(function(parsedEvents){
+            res.send(JSON.stringify(parsedEvents));
+           })
+        }
+    });
+ });
+
+ var saveEventsToFile = function(events,streamid){
+    var deferred = Q.defer();
+    _.each(events,function(event){
+        saveEventToFile(event,streamid);
+    });
+    deferred.resolve(events);
+    return deferred.promise;
+ }
+
+function parseEvents(events,stream){
+    var parsedEvents = [];
+    _.each(events,function(event){
+         parsedEvents.push(parseEvent(event,stream));
+    });
+    return parsedEvents;
+ }
 
 app.get('/reset', function (req, res) {
    fs.remove(__dirname +'/fake_data', function(err){
@@ -90,8 +126,14 @@ app.get('/reset', function (req, res) {
    });
  });
 
+var findStreamById = function(streamid,callback){
+    var fileName = __dirname +'/fake_data/stream/'+streamid;
+    fs.readFile(fileName, 'utf8', function (err,data) {
+        callback(err,JSON.parse(data));
+    });
+}
 
-var parseEvent = function(event, stream, res){
+var parseEvent = function(event, stream){
     var event = {
             _id : new ObjectId(),
             event : {
@@ -115,7 +157,7 @@ function saveEventToFile(event,streamid){
 
     fs.ensureFile(fileName, function(err) {
         if(err){
-            console.log(err); //null
+            console.log(err);
         }else{
             fs.writeFile(fileName, JSON.stringify(event, null, 4), function(err) {
                 if(err) {
@@ -126,25 +168,23 @@ function saveEventToFile(event,streamid){
             });
         }
     });
-
-
 }
 
 var authenticateWriteToken = function (token, id, error, success) {
     var fileName = __dirname +'/fake_data/stream/'+id;
     fs.readFile(fileName, 'utf8', function (err,data) {
-      if (err){
-        error()
-      }
-      else{
-        var stream = JSON.parse(data);
-        if(stream.writeToken != token){
-            error()
+        if (!err){
+            var stream = JSON.parse(data);
+            if(stream.writeToken != token){
+                error()
+            }
+            else{
+                success(stream);
+            }
         }
         else{
-            success(stream);
+            error()
         }
-      }
     });
 };
 
@@ -161,7 +201,6 @@ function randomValueBase64 (len) {
         .replace(/\+/g, '0')  // replace '+' with '0'
         .replace(/\//g, '0'); // replace '/' with '0'
 }
-
 
 var port = 7000;
 app.listen(port, function () {
